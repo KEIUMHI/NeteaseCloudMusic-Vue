d3472601523b2a2c1e5971a2ea52b9fc3090d9f5
keiumhi <1528158315@qq.com>
2019-09-08 22:07:55 +0800

    add swiper tabpage slide

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
   <div id="app">
-    <tab-head />
+
+    <div class="tab-header">
+      <tab-head />
+    </div>
     
-    <div class="swiper-container">
+    <div
+      id="page"
+      class="swiper-container"
+      style="touch-aciton: none;">
       <div class="swiper-wrapper">
-          <!-- <div class="swiper-slide"><Mine /></div>
-          <div class="swiper-slide"><Find /></div>
-          <div class="swiper-slide"><Cloud /></div>
-          <div class="swiper-slide"><Videos /></div> -->
-          <div class="swiper-slide">
-            <keep-alive>
-              <router-view />
-            </keep-alive>
-          </div>
+        <div class="swiper-slide slidepage"><Mine /></div>
+        <div class="swiper-slide slidepage"><Find /></div>
+        <div class="swiper-slide slidepage"><Cloud /></div>
+        <div class="swiper-slide slidepage"><Videos /></div>
       </div>
-      <!-- 如果需要分页器 -->
-      <!-- <div class="swiper-pagination"></div> -->
-      
-      <!-- 如果需要导航按钮 -->
-       <div class="swiper-button-prev"></div>
-       <div class="swiper-button-next"></div>
-      
-      <!-- 如果需要滚动条 -->
-      <!-- <div class="swiper-scrollbar"></div> -->
   </div>
+
 </div>
 </template>
 
@@ -33,15 +26,113 @@ import Mine from './views/mine/index'
 import Find from './views/find/index'
 import Cloud from './views/cloud/index'
 import Videos from './views/videos/index'
-import Swiper from 'swiper';
-import 'swiper/dist/css/swiper.min.css';
+import Swiper from 'swiper'
+import 'swiper/dist/css/swiper.min.css'
+import 'swiper/dist/js/swiper.min.js'
+
 export default {
   name: 'app',
+  data () {
+    return {
+      // bar: null,
+      navSum: 0,
+      navSlideWidth: 0,
+      clientWidth: 0,
+      navWidth: 0
+    }
+  },
   mounted () {
-    var mySwiper = new Swiper('.swiper-container', {})
+    this.$nextTick(() => {
+      const _this = this
+      // let barwidth = 36 //导航粉色条的长度px
+      let tSpeed = 300 //切换速度300ms
+      let navSwiper = new Swiper('#nav', {
+        slidesPerView: 4, // 设置slider容器能够同时显示的slides数量(carousel模式)
+        freeMode: true,
+        on: {
+          init: function() {
+            _this.navSlideWidth = this.slides.eq(0).css('width') //导航字数需要统一,每个导航宽度一致
+            // _this.bar = this.$el.find('.bar')
+            // _this.bar.css('width', _this.navSlideWidth)
+            // _this.bar.transition(tSpeed)
+            _this.navSum = this.slides[this.slides.length - 1].offsetLeft //最后一个slide的位置
+  
+            _this.clientWidth = parseInt(this.$wrapperEl.css('width')) //Nav的可视宽度
+            _this.navWidth = this.width
+            // for (let i = 0; i < this.slides.length; i++) {
+            //   _this.navWidth += parseInt(this.slides.eq(i).css('width'))
+            // }
+  
+            // let topBar = this.$el.parents('body').find('#top') //页头
+  
+          },
+  
+        },
+      });
+      // eslint-disable-next-line
+      let pageSwiper = new Swiper('#page', {
+        watchSlidesProgress: true, // 允许 watch SlidesProgress
+        resistanceRatio: 0,
+        initialSlide : 1, // 设置起始页
+        on: {
+          touchMove: function() {
+            // let progress = this.progress
+            // _this.bar.transition(0)
+            // _this.bar.transform('translateX(' + _this.navSum * progress + 'px)')
+            //粉色255,72,145灰色51,51,51
+            for (let i = 0; i < this.slides.length; i++) {
+              let slideProgress = this.slides[i].progress
+              if (Math.abs(slideProgress) < 1) {
+                let r = Math.floor((51 - 153) * (1 - Math.pow(Math.abs(slideProgress), 2)) + 153)
+                let g = Math.floor((51 - 153) * (1 - Math.pow(Math.abs(slideProgress), 2)) + 153)
+                let b = Math.floor((51 - 153) * (1 - Math.pow(Math.abs(slideProgress), 2)) + 153)
+                navSwiper.slides.eq(i).find('span').css('color', 'rgba(' + r + ',' + g + ',' + b + ',1)')
+                let size = Math.floor((16 - 14) * (1 - Math.pow(Math.abs(slideProgress), 2)) + 14)
+                navSwiper.slides.eq(i).find('span').css('font-size', size + 'px')
+              }
+            }
+          },
+          transitionStart: function() {
+            let activeIndex = this.activeIndex
+            // let activeSlidePosition = navSwiper.slides[activeIndex].offsetLeft
+            //释放时导航粉色条移动过渡
+            // _this.bar.transition(tSpeed)
+            // _this.bar.transform('translateX(' + activeSlidePosition + 'px)')
+            //释放时文字变色过渡
+            navSwiper.slides.eq(activeIndex).find('span').transition(tSpeed)
+            navSwiper.slides.eq(activeIndex).find('span').css('color', 'rgba(51,51,51,1)')
+            navSwiper.slides.eq(activeIndex).find('span').css('font-size', '16px')
+            navSwiper.slides.eq(activeIndex).find('span').css('font-weight', 'bold')
+            if (activeIndex > 0) {
+              navSwiper.slides.eq(activeIndex - 1).find('span').transition(tSpeed)
+              navSwiper.slides.eq(activeIndex - 1).find('span').css('color', '#999')
+              navSwiper.slides.eq(activeIndex - 1).find('span').css('font-size', '14px')
+              navSwiper.slides.eq(activeIndex - 1).find('span').css('font-weight', 'normal')
+            }
+            if (activeIndex < this.slides.length) {
+              navSwiper.slides.eq(activeIndex + 1).find('span').transition(tSpeed)
+              navSwiper.slides.eq(activeIndex + 1).find('span').css('color', '#999')
+              navSwiper.slides.eq(activeIndex + 1).find('span').css('font-size', '14px')
+              navSwiper.slides.eq(activeIndex + 1).find('span').css('font-weight', 'normal')
+            }
+            //导航居中
+            let navActiveSlideLeft = navSwiper.slides[activeIndex].offsetLeft //activeSlide距左边的距离
+  
+            navSwiper.setTransition(tSpeed)
+            if (navActiveSlideLeft < (_this.clientWidth - parseInt(_this.navSlideWidth)) / 2) {
+              navSwiper.setTranslate(0)
+            } else if (navActiveSlideLeft > _this.navWidth - (parseInt(_this.navSlideWidth) + _this.clientWidth) / 2) {
+              navSwiper.setTranslate(_this.clientWidth - _this.navWidth)
+            } else {
+              navSwiper.setTranslate((_this.clientWidth - parseInt(_this.navSlideWidth)) / 2 - navActiveSlideLeft)
+            }
+  
+          },
+        }
+      })
+    })
   },
   methods: {
-
   },
   components: {
     TabHead,
@@ -55,12 +146,7 @@ export default {
 
 <style lang="less">
   @import "~@/assets/style/variables.less";
-  // #app {
-  //   -webkit-text-size-adjust:none;
-  // }
-  .swiper-container {
-    width: 100vw;
-    height: 100vh;
+  .tab-header {
+    margin: 8px 0 0 0;
   }
-
 </style>
--- src/components/tab-head/tab-head.vue
+++ src/components/tab-head/tab-head.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="wrap">
     <div class="tab-head">
-      <div>
-        <i class="iconfont iconMenu"></i>
-      </div>
+      <div><i class="iconfont iconMenu"></i></div>
       <div class="options">
-        <router-link
+        <!-- <router-link
           tag="a"
           to="/mine"
           class="tab-item">我的</router-link>
@@ -20,9 +18,26 @@
         <router-link
           tag="a"
           to="/videos"
-          class="tab-item">视频</router-link>
+          class="tab-item">视频</router-link> -->
+        <div
+          id="nav"
+          class="swiper-container swiper-no-swiping">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <span>我的</span></div>
+            <div class="swiper-slide">
+              <span style="font-size: 16px; font-weight: bold; color:rgba(51,51,51,1);">发现</span></div>
+            <div class="swiper-slide">
+              <span>云村</span></div>
+            <div class="swiper-slide">
+              <span>视频</span></div>
+            <!-- <div class="bar">
+              <div class="color"></div>
+            </div> -->
+          </div>
+        </div>
       </div>
-      <div></div>
+      <div><i class="iconfont iconsearch"></i></div>
     </div>
   </div>
 </template>
@@ -52,17 +67,38 @@ export default {
     justify-content: space-around;
     width: 60%;
     
-    .tab-item {
-      width: 25%;
-      font-size: @font-size-medium;
-      text-align: center;
-      text-decoration: none;
-      color: #999;
+    // .tab-item {
+    //   width: 25%;
+    //   font-size: @font-size-medium;
+    //   text-align: center;
+    //   text-decoration: none;
+    //   color: #999;
 
-      &.router-link-active {
-        font-size: @font-size-medium-x;
-        font-weight: bold;
-        color: #333;
+    //   &.router-link-active {
+    //     font-size: @font-size-medium-x;
+    //     font-weight: bold;
+    //     color: #333;
+    //   }
+    // }
+
+    #nav {
+      width: 100%;
+
+      .swiper-wrapper {
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+      }
+
+      .swiper-slide {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        // margin: 0 2px;
+        width: 25%;
+        font-size: @font-size-medium;
+        text-align: center;
+        color: #999;
       }
     }
   }
