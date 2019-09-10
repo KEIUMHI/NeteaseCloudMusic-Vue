7645170dba9dbbde5b8767bf183631e31daf7c7d
keiumhi <1528158315@qq.com>
2019-09-10 17:55:59 +0800

    modify box-show

--- src/components/box-show/box-show.vue
+++ src/components/box-show/box-show.vue
@@ -1,17 +1,86 @@
 <template>
   <div class="box-show">
-    <div class="img_bg-container">
+    <div class="img_bg-container d-flex">
+      <img
+        class="img"
+        alt="recommend-music_img"
+        :src="picUrl" />
+        <PlayCount
+          v-if="playCount"
+          class="paly-count_position"
+          :count="playCount"
+        />
     </div>
-    <p>这些都是文字内容呐这些都是文字内容呐这些都是文字内容呐</p>
+    <p class="name">{{ name }}</p>
   </div>
 </template>
 
 <script>
+// COMPONENTS
+import PlayCount from '../play-count/play-count'
+
 export default {
-  name: 'box-show'
+  name: 'box-show',
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    picUrl: {
+      type: String,
+      required: true
+    },
+    copywriter: {
+      type: String,
+      default: null
+    },
+    playCount: {
+      type: [String, Number],
+      default: null
+    },
+    styles: {
+      type: Object,
+      default: () => {
+        return {
+          width: '100px',
+          heigth: '100px'
+        }
+      }
+    }
+  },
+  components: {
+    PlayCount
+  }
 }
 </script>
 
 <style lang="less" scoped>
+  @import "~@/assets/style/variables.less";
+  .box-show {
+    width: calc((100vw - 28px - 20px) / 3);
 
+    .img_bg-container {
+      position: relative;
+
+      .img {
+        border-radius: 4px;
+        width: 100%;
+      }
+
+      .paly-count_position {
+        position: absolute;
+        top: 3px;
+        right: 3px;
+      }
+    }
+  }
+
+  .name {
+    font-size: @font-size-mini-l;
+    color: @color-font-default;
+  }
 </style>
