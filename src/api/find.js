ee84ebd35051e1144cd7274b2042311ca5b2a733
keiumhi <1528158315@qq.com>
2019-09-10 17:53:57 +0800

    add getRecommendMusic

--- src/api/find.js
+++ src/api/find.js
@@ -1,11 +1,18 @@
 import request from '@/utils/request'
-
+// 获取轮播
 export function getBanner (query) {
   return request({
     url: 'banner',
     method: 'get',
-    prams: query
+    params: query
   })
 }
-
+// 获取推荐音乐
+export function getRecommendMusic (query) {
+  return request({
+    url: 'personalized',
+    method: 'get',
+    params: query
+  })
+}
 
