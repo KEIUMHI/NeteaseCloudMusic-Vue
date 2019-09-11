8e89e1825fff9dfeca1669a2e1c68eb7e90f1b7f
keiumhi <1528158315@qq.com>
2019-09-11 16:07:52 +0800

    add getRecommendNewSong&getNewSong&getNewAlbum

--- src/api/find.js
+++ src/api/find.js
@@ -15,4 +15,27 @@ export function getRecommendMusic (query) {
     params: query
   })
 }
-
+// 获取推荐新音乐
+export function getRecommendNewSong (query) {
+  return request({
+    url: 'personalized/newsong',
+    method: 'get',
+    params: query
+  })
+}
+// 新碟推送
+export function getNewAlbum (query) {
+  return request({
+    url: 'top/album',
+    method: 'get',
+    params: query
+  })
+}
+// 新歌速递 type 0全部 7华语 96欧美 8日本 16韩国
+export function getNewSong (query) {
+  return request({
+    url: 'top/song',
+    method: 'get',
+    params: query
+  })
+}
