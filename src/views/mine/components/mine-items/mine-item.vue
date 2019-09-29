0a247b47c24308dfa9164d12102a337c589c15d2
keiumhi <1528158315@qq.com>
2019-09-29 21:58:01 +0800

    mine-item TS

---
+++ src/views/mine/components/mine-items/mine-item.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mine-item">
+    <div class="icon"></div>
+    <div class="name-wrapper">
+      <div class="name">{{ name }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mine-item',
+  props: {
+    id: {
+      type: Number,
+      default: 0
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .mine-item {
+    padding: 15px 0;
+  }
+
+  .icon {
+    margin: 0 20px;
+    width: 30px;
+  }
+
+  .name-wrapper {
+    border-bottom: 1px solid #777;
+  }
+</style>
---
+++ src/views/mine/components/mine-items/mine-items.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mine-items">
+    <MineItem
+      v-for="mineItem in mineItems" :key="mineItem.id"
+      :id="mineItem.id"
+      :name="mineItem.nm"
+      :icon="mineItem.icon"
+    />
+  </div>
+</template>
+
+<script>
+import MineItem from './mine-item'
+
+export default {
+  name: 'mine-items',
+  data () {
+    return {
+      mineItems: [{
+        id: 1,
+        nm: '本地音乐',
+        icon: ''
+      }, {
+        id: 2,
+        nm: '最近播放',
+        icon: ''
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
